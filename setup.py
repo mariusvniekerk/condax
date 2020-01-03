4c9bda301c11681edd7946786b37f9ab68be893e
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-01-03 09:26:21 +0200

    bumped version to 0.0.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ REQUIRES = ["click", "requests", "userpath", "PyYAML"]
 
 setup(
     name="condax",
-    version="0.0.2",
+    version="0.0.3",
     description="Install and run applications packaged with conda in isolated environments",
     long_description=readme,
     author="Marius van Niekerk",
