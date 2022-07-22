d0665c8e12567c0b92950dfb954741a55e0984ba
Yamato Matsuoka <yamaton@gmail.com>
2022-07-21 23:14:14 -0400

    Add test of config and environment variables
    The filename starts with _ because the test works by itself,
    but it fails when executed together with other tests.

---
+++ tests/_test_env_vars.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pytest
+
+
+@pytest.fixture
+def mock_env_1(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setenv("CONDAX_PREFIX_DIR", "/a/s/df/ghgg")
+    monkeypatch.setenv("CONDAX_BIN_DIR", "~/yuip/../.hhh/kkk")
+    monkeypatch.setenv("CONDAX_CHANNELS", "fastchan  monorepo  baba")
+
+
+# Test if C loads environment variables
+def test_loading_env(mock_env_1):
+    from condax.config import C
+    assert C.prefix_dir() == Path("/a/s/df/ghgg")
+    assert C.bin_dir() == Path.home() / ".hhh/kkk"
+    assert C.channels() == ["fastchan", "monorepo", "baba"]
