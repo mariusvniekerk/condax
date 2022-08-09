a15f22abdbc951620fa2e9867ba2ef4ebaf70ee0
Yamato Matsuoka <yamaton@gmail.com>
2022-08-09 14:59:05 -0400

    Revert "Don't use micromamba on windows for now"
    This reverts commit c2686baca9ee338171a5bf65af3ec5618bf5cc46.

--- condax/conda.py
+++ condax/conda.py
@@ -30,9 +30,6 @@ def ensure_conda() -> Path:
 
 
 def ensure_micromamba() -> Path:
-    if os.name == "nt":
-        return ensure_conda()
-
     execs = ["micromamba"]
     for conda_exec in execs:
         conda_path = shutil.which(conda_exec)
