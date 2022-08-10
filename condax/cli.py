af95d82edbf19de1beafca4edb0b554973d45a02
Abraham Murciano <abraham.murciano@mobileye.com>
2022-08-10 17:23:47 +0300

    Added --link-conflict flag
    With this flag the user can specify what to do when a link already
    exists in the ~/.local/bin directory. The default is `error`, which
    will cause the install to fail. The other options are `skip` and
    `overwrite`.

--- condax/cli.py
+++ condax/cli.py
@@ -13,7 +13,7 @@ def cli():
 @cli.command(
     help=f"""
     Install a package with condax.
-    
+
     This will install a package into a new conda environment and link the executable
     provided by it to `{config.CONDAX_LINK_DESTINATION}`.
     """
@@ -22,20 +22,35 @@ def cli():
     "--channel",
     "-c",
     multiple=True,
-    help=f"""Use the channels specified to install.  If not specified condax will 
+    help=f"""Use the channels specified to install.  If not specified condax will
     default to using {config.DEFAULT_CHANNELS}.""",
 )
+@click.option(
+    "--link-conflict",
+    "-l",
+    default="error",
+    type=click.Choice([action.value for action in core.LinkConflictAction]),
+    help=f"""How to handle conflicts when a link with the same name already exists in
+    `{config.CONDAX_LINK_DESTINATION}`.  If `error` is specified, condax will exit with
+    an error if the link already exists.  If `overwrite` is specified, condax will
+    overwrite the existing link.  If `skip` is specified, condax will skip linking the
+    conflicting executable.""",
+)
 @click.argument("package")
-def install(channel, package):
+def install(channel, package, link_conflict):
     if channel is None or (len(channel) == 0):
         channel = config.DEFAULT_CHANNELS
-    core.install_package(package, channels=channel)
+    core.install_package(
+        package,
+        channels=channel,
+        link_conflict_action=core.LinkConflictAction(link_conflict)
+    )
 
 
 @cli.command(
     help="""
     Remove a package installed by condax.
-    
+
     This will remove a package installed with condax and destroy the underlying
     conda environment.
     """
@@ -58,20 +73,31 @@ def ensure_path():
 @cli.command(
     help="""
     Update package(s) installed by condax.
-    
+
     This will update the underlying conda environments(s) to the latest release of a package.
 """
 )
 @click.option(
     "--all", is_flag=True, help="Set to update all packages installed by condax"
 )
+@click.option(
+    "--link-conflict",
+    "-l",
+    default="error",
+    type=click.Choice([action.value for action in core.LinkConflictAction]),
+    help=f"""How to handle conflicts when a link with the same name already exists in
+    `{config.CONDAX_LINK_DESTINATION}`.  If `error` is specified, condax will exit with
+    an error if the link already exists.  If `overwrite` is specified, condax will
+    overwrite the existing link.  If `skip` is specified, condax will skip linking the
+    conflicting executable.""",
+)
 @click.argument("package", default="", required=False)
 @click.pass_context
-def update(ctx, all, package):
+def update(ctx, all, link_conflict, package):
     if all:
-        core.update_all_packages()
+        core.update_all_packages(core.LinkConflictAction(link_conflict))
     elif package:
-        core.update_package(package)
+        core.update_package(package, core.LinkConflictAction(link_conflict))
     else:
         print(ctx.get_help(), file=sys.stderr)
 
---
+++ news/link-conflict.md
@@ -0,0 +1,5 @@
+### Added:
+
+* `--link-conflict` flag to specify what to do when a link already exists in the
+  ~/.local/bin directory. The default is `error`, which will cause the install to
+  fail. The other options are `skip` and `overwrite`.
